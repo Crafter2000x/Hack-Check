8ffbe74b2f4100e42a85e4c12b1115009d0d33f5
Connor Timmerman <457736@student.fontys.nl>
2021-03-01 16:53:04 +0100

    initial commit

---
+++ Hack Check/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Hack_Check.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
